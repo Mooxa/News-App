126ef4facbc6a84b5ef271c89fbebdffb6a34f59
Mouhamed Dieye <dieyem93@gmail.com>
2021-05-21 15:46:33 +0000

    Initial Commit

---
+++ Shared/XibaarApp.swift
@@ -0,0 +1,17 @@
+//
+//  XibaarApp.swift
+//  Shared
+//
+//  Created by Mouhamed Dieye on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct XibaarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
